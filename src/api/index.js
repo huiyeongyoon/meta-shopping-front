6abe321f07ba23d5057e21ae0169779a6c831c02
huiyeong <zkdl2960@naver.com>
2022-02-28 14:16:41 +0900

    push

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 // 1. HTTP resquest response 기본 설정
 const config = {
   // baseUrl: 'https://api.hnpwa.com/v0/news/1.json'
-  baseUrl: 'http://localhost:3000/'
+  baseUrl: 'http://localhost:3000'
 }
 
 // 2. api 함수들 정리
 function fetchProductList() {
-  return axios.get(`${config.baseUrl}`)
+  return axios.get(`${config.baseUrl}/product`)
 }
 
 export { fetchProductList }

2103552b6938d5ebd0f6ef83349462fc90afdfb0
ctg620 <87960350+turtlecpp@users.noreply.github.com>
2022-03-03 17:39:12 +0900

    0303

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -21,158 +21,184 @@ const stateInit = {
   }
 }
 
-export default {
-  state: {
-    UserList: [],
-    User: { ...stateInit.User },
-    InsertedResult: null,
-    UpdatedResult: null,
-    DeletedResult: null,
-    InputMode: null
+const state = {
+  UserList: [],
+  User: { ...stateInit.User },
+  InsertedResult: null,
+  UpdatedResult: null,
+  DeletedResult: null,
+  InputMode: null,
+  IdCheck: false
+}
+
+const getters = {
+  UserList: state => state.UserList,
+  User: state => state.User,
+  UserInsertedResult: state => state.InsertedResult,
+  UserUpdatedResult: state => state.UpdatedResult,
+  UserDeletedResult: state => state.DeletedResult,
+  UserInputMode: state => state.InputMode,
+  Idcheck: state => state.Idcheck
+}
+
+const mutations = {
+  setUserRole(state, data) {
+    state.User.userRole = data
   },
-  getters: {
-    UserList: state => state.UserList,
-    User: state => state.User,
-    UserInsertedResult: state => state.InsertedResult,
-    UserUpdatedResult: state => state.UpdatedResult,
-    UserDeletedResult: state => state.DeletedResult,
-    UserInputMode: state => state.InputMode
+  setUserGrade(state, data) {
+    state.User.userGrade = data
   },
-  mutations: {
-    setUserRole(state, data) {
-      state.User.userRole = data
-    },
-    setUserGrade(state, data) {
-      state.User.userGrade = data
-    },
-    setUserList(state, data) {
-      state.UserList = data
-    },
-    setUser(state, data) {
-      state.User = data
-    },
-    setInsertedResult(state, data) {
-      state.InsertedResult = data
-    },
-    setUpdatedResult(state, data) {
-      state.UpdatedResult = data
-    },
-    setDeletedResult(state, data) {
-      state.DeletedResult = data
-    },
-    setInputMode(state, data) {
-      state.InputMode = data
-    }
+  setUserList(state, data) {
+    state.UserList = data
   },
-  actions: {
-    //유저 권한 및 등급 설정
-    actUserRole(context, payload) {
-      //초기화
-      context.commit('setUser', { ...stateInit.User })
-
-      console.log('setUserRole :', payload)
-      context.commit('setUserRole', payload)
-      console.log('setUserRole2 :', this.state.User.User.userRole)
-      if (payload === 1) {
-        context.commit('setUserGrade', '일반 구매자')
-      } else {
-        context.commit('setUserGrade', '관리자')
-      }
-    },
-    // 리스트 조회
-    actUserList(context, payload) {
-      /* RestAPI 호출 */
-      api
-        .get('/api/users', { params: payload })
-        .then(response => {
-          const userList = response && response.data && response.data.rows
-          context.commit('setUserList', userList)
-        })
-        .catch(error => {
-          // 에러인 경우 처리
-          console.error('UserList.error', error)
-          context.commit('setUserList', [])
-        })
-    },
-    // 등록
-    actUserInsert(context, payload) {
-      // 상태값 초기화
-      context.commit('setInsertedResult', null)
-
-      /* RestAPI 호출 */
-      api
-        .post('/api/users', payload)
-        .then(response => {
-          const insertedResult = response && response.data && response.data.id
-          context.commit('setInsertedResult', insertedResult)
-        })
-        .catch(error => {
-          // 에러인 경우 처리
-          console.error('UserInsert.error', error)
-          context.commit('setInsertedResult', -1)
-        })
-    },
-    // 초기화
-    actUserInit(context, payload) {
-      context.commit('setUser', { ...stateInit.User })
-    },
-    // 입력모드
-    actUserInputMode(context, payload) {
-      context.commit('setInputMode', payload)
-    },
-    // 상세정보 조회
-    actUserInfo(context, payload) {
-      // 상태값 초기화
-      context.commit('setUser', { ...stateInit.User })
-
-      /* RestAPI 호출 */
-      api
-        .get(`/api/users/${payload}`)
-        .then(response => {
-          const user = response && response.data
-          context.commit('setUser', user)
-        })
-        .catch(error => {
-          // 에러인 경우 처리
-          console.error('UserInfo.error', error)
-          context.commit('setUser', -1)
-        })
-    },
-    // 수정
-    actUserUpdate(context, payload) {
-      // 상태값 초기화
-      context.commit('setUpdatedResult', null)
-
-      /* RestAPI 호출 */
-      api
-        .put(`/api/users/${payload.id}`, payload)
-        .then(response => {
-          const updatedResult = response && response.data && response.data.updatedCount
-          context.commit('setUpdatedResult', updatedResult)
-        })
-        .catch(error => {
-          // 에러인 경우 처리
-          console.error('UserUpdate.error', error)
-          context.commit('setUpdatedResult', -1)
-        })
-    },
-    // 삭제
-    actUserDelete(context, payload) {
-      // 상태값 초기화
-      context.commit('setDeletedResult', null)
-
-      /* RestAPI 호출 */
-      api
-        .delete(`/api/users/${payload}`)
-        .then(response => {
-          const deletedResult = response && response.data && response.data.deletedCount
-          context.commit('setDeletedResult', deletedResult)
-        })
-        .catch(error => {
-          // 에러인 경우 처리
-          console.error('UserDelete.error', error)
-          context.commit('setDeletedResult', -1)
-        })
-    }
+  setUser(state, data) {
+    state.User = data
+  },
+  setInsertedResult(state, data) {
+    state.InsertedResult = data
+  },
+  setUpdatedResult(state, data) {
+    state.UpdatedResult = data
+  },
+  setDeletedResult(state, data) {
+    state.DeletedResult = data
+  },
+  setInputMode(state, data) {
+    state.InputMode = data
+  },
+  setIdCheck(state, data) {
+    state.IdCheck = data
   }
+}
+
+const actions = {
+  //유저 권한 및 등급 설정
+  actUserRole(context, payload) {
+    //초기화
+    context.commit('setUser', { ...stateInit.User })
+
+    console.log('setUserRole :', payload)
+    context.commit('setUserRole', payload)
+    console.log('setUserRole2 :', this.state.User.User.userRole)
+    if (payload === 1) {
+      context.commit('setUserGrade', '일반 구매자')
+    } else {
+      context.commit('setUserGrade', '관리자')
+    }
+  },
+  // 리스트 조회
+  actUserList(context, payload) {
+    /* RestAPI 호출 */
+    api
+      .get('/api/users', { params: payload })
+      .then(response => {
+        const userList = response && response.data
+        console.log('act : ', response.data)
+        context.commit('setUserList', userList)
+      })
+      .catch(error => {
+        // 에러인 경우 처리
+        console.error('UserList.error', error)
+        context.commit('setUserList', [])
+      })
+  },
+  // 등록
+  actUserInsert(context, payload) {
+    // 상태값 초기화
+    context.commit('setInsertedResult', null)
+
+    /* RestAPI 호출 */
+    api
+      .post('/api/users', payload)
+      .then(response => {
+        const insertedResult = response && response.data && response.data.id
+        context.commit('setInsertedResult', insertedResult)
+      })
+      .catch(error => {
+        // 에러인 경우 처리
+        console.error('UserInsert.error', error)
+        context.commit('setInsertedResult', -1)
+      })
+  },
+  // 초기화
+  actUserInit(context, payload) {
+    context.commit('setUser', { ...stateInit.User })
+  },
+  // 입력모드
+  actUserInputMode(context, payload) {
+    context.commit('setInputMode', payload)
+  },
+  // 상세정보 조회
+  actUserInfo(context, payload) {
+    // 상태값 초기화
+    context.commit('setUser', { ...stateInit.User })
+
+    /* RestAPI 호출 */
+    api
+      .get(`/api/users/${payload}`)
+      .then(response => {
+        const user = response && response.data
+        context.commit('setUser', user)
+      })
+      .catch(error => {
+        // 에러인 경우 처리
+        console.error('UserInfo.error', error)
+        context.commit('setUser', -1)
+      })
+  },
+  // 수정
+  actUserUpdate(context, payload) {
+    // 상태값 초기화
+    context.commit('setUpdatedResult', null)
+
+    /* RestAPI 호출 */
+    api
+      .put(`/api/users/${payload.id}`, payload)
+      .then(response => {
+        const updatedResult = response && response.data && response.data.updatedCount
+        context.commit('setUpdatedResult', updatedResult)
+      })
+      .catch(error => {
+        // 에러인 경우 처리
+        console.error('UserUpdate.error', error)
+        context.commit('setUpdatedResult', -1)
+      })
+  },
+  // 삭제
+  actUserDelete(context, payload) {
+    // 상태값 초기화
+    context.commit('setDeletedResult', null)
+
+    /* RestAPI 호출 */
+    api
+      .delete(`/api/users/${payload}`)
+      .then(response => {
+        const deletedResult = response && response.data && response.data.deletedCount
+        context.commit('setDeletedResult', deletedResult)
+      })
+      .catch(error => {
+        // 에러인 경우 처리
+        console.error('UserDelete.error', error)
+        context.commit('setDeletedResult', -1)
+      })
+  },
+  actUserIdcheck(context, payload) {
+    /* RestAPI 호출 */
+    api
+      .post(`/api/users/idcheck`, payload)
+      .then(response => {
+        context.commit('setIdCheck', response)
+      })
+      .catch(error => {
+        // 에러인 경우 처리
+        console.error('Useridcheck.error', error)
+      })
+  }
+}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
 }
--- src/views/signup/SignUp2.vue
+++ src/views/signup/SignUp2.vue
@@ -9,10 +9,26 @@
           <br />
           <br />
           <b-form-group label-cols="3" label="아이디" label-for="id">
-            <b-form-input id="id" v-model="user.userId"></b-form-input>
+            <b-input-group>
+              <b-form-input id="id" v-model="user.userId"></b-form-input>
+              <b-input-group-append>
+                <b-button variant="primary" @click="idCheck">아이디 중복확인</b-button>
+              </b-input-group-append>
+            </b-input-group>
           </b-form-group>
           <b-form-group label-cols="3" label="비밀번호" label-for="password">
-            <b-form-input id="password" v-model="user.userPassword" type="password"></b-form-input>
+            <b-form-input
+              id="password"
+              v-model="user.userPassword"
+              type="password"
+              placeholder="8자 이상 넣으세요"
+            ></b-form-input>
+            <b-input-group>
+              <b-form-input id="password2" v-model="userPassword2" type="password"></b-form-input>
+              <b-input-group-append>
+                <b-button variant="primary" @click="passwordCheck">비밀번호 확인</b-button>
+              </b-input-group-append>
+            </b-input-group>
           </b-form-group>
           <b-form-group label-cols="3" label="이름" label-for="name">
             <b-form-input id="name" v-model="user.userName"></b-form-input>
@@ -21,7 +37,7 @@
             <b-form-input id="nickname" v-model="user.userNickname"></b-form-input>
           </b-form-group>
           <b-form-group label-cols="3" label="성별" label-for="role">
-            <b-form-radio-group v-model="user.userGender" :options="userGender.options"></b-form-radio-group>
+            <b-form-radio-group v-model="user.usergender" :options="usergender.options"></b-form-radio-group>
           </b-form-group>
           <b-form-group label-cols="3" label="사용자권한" label-for="role">
             <b-form-radio-group
@@ -35,19 +51,19 @@
             <b-form-input id="email" v-model="user.userEmail"></b-form-input>
           </b-form-group>
           <b-form-group label-cols="3" label="이메일 수신동의" label-for="role">
-            <b-form-radio-group v-model="user.emailCheck" :options="emailCheck.options"></b-form-radio-group>
+            <b-form-radio-group v-model="user.emailcheck" :options="emailcheck.options"></b-form-radio-group>
           </b-form-group>
           <b-form-group label-cols="3" label="전화번호" label-for="phone">
             <b-form-input id="phone" v-model="user.userPhone"></b-form-input>
           </b-form-group>
           <b-form-group label-cols="3" label="주소" label-for="address">
             <b-input-group>
-              <b-form-input id="address1" v-model="user.userAddress1" placeholder="도로주소" disabled></b-form-input>
+              <b-form-input id="address1" v-model="user.userAddress1" placeholder="우편번호" disabled></b-form-input>
               <b-input-group-append>
                 <b-button variant="primary" @click="clickAddress">주소검색</b-button>
               </b-input-group-append>
             </b-input-group>
-            <b-form-input id="address2" v-model="user.userAddress2" placeholder="건물명" disabled></b-form-input>
+            <b-form-input id="address2" v-model="user.userAddress2" placeholder="도로주소" disabled></b-form-input>
             <b-form-input
               id="address3"
               v-model="user.userAddress3"
@@ -80,7 +96,7 @@ export default {
         userProfile: null,
         userNickname: null,
         userRole: this.$store.getters.User.userRole || localStorage.getItem('userRole'),
-        emailCheck: null,
+        emailcheck: null,
         userGrade: null,
         userAddress1: null,
         userAddress2: null,
@@ -88,29 +104,32 @@ export default {
         updatedPwDate: null,
         createdAt: null
       },
-      checkPassword: null,
       userRole: {
         options: [
           { value: 1, text: '일반 회원' },
           { value: 2, text: '관리자' }
         ]
       },
-      emailCheck: {
+      emailcheck: {
         options: [
           { value: 'y', text: '동의' },
           { value: 'n', text: '비동의' }
         ]
       },
-      userGender: {
+      usergender: {
         options: [
           { value: 1, text: '남자' },
           { value: 2, text: '여자' }
         ]
-      }
+      },
+      passwordcheck: null,
+      userPassword2: null,
+      idcheck: false
     }
   },
   watch: {},
   created() {
+    this.$store.dispatch('actUserList')
     if (
       localStorage.getItem('userRole') !== this.$store.getters.User.userRole &&
       this.$store.getters.User.userRole !== null
@@ -125,16 +144,16 @@ export default {
   methods: {
     submit() {
       console.log('submit: ', this.user.userId)
-      if (!this.user.userId) {
-        this.$bvToast.toast('아이디를 입력하세요.', {
-          title: 'Fail',
-          variant: 'danger',
-          solid: true
-        })
-        return 0
-      }
-      if (!this.user.userPassword) {
-        this.$bvToast.toast('비밀번호를 입력하세요.', {
+      // if (!this.idcheck) {
+      //   this.$bvToast.toast('아이디 중복확인을 해주세요.', {
+      //     title: 'Fail',
+      //     variant: 'danger',
+      //     solid: true
+      //   })
+      //   return 0
+      // }
+      if (!this.passwordcheck) {
+        this.$bvToast.toast('비밀번호를 확인 및 입력해주세요', {
           title: 'Fail',
           variant: 'danger',
           solid: true
@@ -157,7 +176,7 @@ export default {
         })
         return 0
       }
-      if (!this.user.userGender) {
+      if (!this.user.usergender) {
         this.$bvToast.toast('성별을 체크하세요.', {
           title: 'Fail',
           variant: 'danger',
@@ -173,7 +192,7 @@ export default {
         })
         return 0
       }
-      if (!this.user.emailCheck) {
+      if (!this.user.emailcheck) {
         this.$bvToast.toast('이메일 체크를 해주세요.', {
           title: 'Fail',
           variant: 'danger',
@@ -214,11 +233,48 @@ export default {
       new window.daum.Postcode({
         oncomplete: function (data) {
           console.log(data)
-          that.user.userAddress1 = data.roadAddress
-          that.user.userAddress2 = data.buildingName
+          that.user.userAddress1 = data.zonecode
+          that.user.userAddress2 = data.roadAddress
           // localStorage.setItem('address1', data)
         }
       }).open()
+    },
+    idCheck() {
+      this.$store.dispatch('actUserIdcheck', this.user.userId)
+      if (this.$store.getters.idcheck) {
+        this.$bvToast.toast('해당 아이디는 사용가능합니다.', {
+          title: '확인되었습니다.',
+          variant: 'success',
+          solid: true
+        })
+        this.idcheck = true
+      } else {
+        this.$bvToast.toast('중복된 아이디가 있습니다.', {
+          title: '사용 불가능 합니다.',
+          variant: 'danger',
+          solid: true
+        })
+        this.idcheck = false
+      }
+    },
+    passwordCheck() {
+      if (String(this.user.userPassword).length > 7) {
+        if (this.user.userPassword === this.userPassword2) {
+          this.$bvToast.toast('비빌번호 확인 완료했습니다', {
+            title: '사용가능합니다.',
+            variant: 'success',
+            solid: true
+          })
+          this.passwordcheck = true
+          return 0
+        }
+      }
+      this.$bvToast.toast('비밀번호 확인 부탁드립니다.', {
+        title: '실패하였습니다.',
+        variant: 'danger',
+        solid: true
+      })
+      this.passwordcheck = false
     }
   }
 }
